50880b730ef82060384ef641943919b2e02be127
Ricardo <keksklauer4@gmail.com>
2023-04-30 22:09:41 +0200

    Added assignment class

---
+++ src/common/misc.hpp
@@ -0,0 +1,13 @@
+#ifndef __GD_MISC_HPP__
+#define __GD_MISC_HPP__
+
+#include <gd_types.hpp>
+
+namespace gd
+{
+
+  inline bool isDefined(size_t id) { return id != UINT_UNDEF; }
+
+}
+
+#endif
--- src/common/pointset.hpp
+++ src/common/pointset.hpp
@@ -9,9 +9,14 @@ namespace gd
   class PointSet
   {
     public:
+      using point_iterator_t = Vector<coordinate_t>::const_iterator;
+      using point_iterator_pair_t = std::pair<point_iterator_t, point_iterator_t>;
+    public:
       PointSet(){}
       PointSet(Vector<coordinate_t>&& points): m_points(points) {}
 
+      point_iterator_pair_t getPointIterator() const
+      { return std::make_pair(m_points.begin(), m_points.end()); }
       size_t getNumPoints() const { return m_points.size(); }
 
     private:
