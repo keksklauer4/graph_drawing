e8a4ceca72028e96800d549274b55372ead21b2d
Ricardo <keksklauer4@gmail.com>
2023-04-30 23:21:53 +0200

    Fix test config.

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,9 @@
 
-#cd external/
-#curl http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz --output lemon.tar.gz
-#tar -xvzf lemon.tar.gz
-#mv lemon-1.3.1/ lemon
-#cd ..
+cd external/
+curl http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz --output lemon.tar.gz
+tar -xvzf lemon.tar.gz
+mv lemon-1.3.1/ lemon
+cd ..
 
 
 cd test
