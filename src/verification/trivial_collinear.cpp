c04ca9413f5b6bcec55ef11080ca99ba6a906a84
Keksklauer4 <keksklauer4@gmail.com>
2023-05-09 12:58:25 +0200

    Fixed bugs and added trivial tests for crossings and collinearity.

--- src/verification/trivial_collinear.cpp
+++ src/verification/trivial_collinear.cpp
@@ -1,11 +1,13 @@
 #include "collinear.hpp"
-#include "common/assignment.hpp"
-#include "gd_types.hpp"
+
+#include <common/assignment.hpp>
 #include <gd_types.hpp>
 
 #include <numeric>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Point_set_2.h>
+
+#define SCALE 2
 
 using namespace gd;
 
@@ -18,35 +20,49 @@ typedef UnorderedMap<coordinate_2d_t, point_id_t, PairHashFunc<coordinate_t>> Co
 struct FakeIterator
 {
   public:
-    FakeIterator(const VertexAssignment& as, const CoordinateToIdMap& m)
-      : m_assignment(as), m_coordinateToId(m), m_failed(false) {}
+    FakeIterator(const VertexAssignment& as, const CoordinateToIdMap& m, bool& failed)
+      : m_assignment(as), m_coordinateToId(m), m_failed(failed) {}
 
 
-    void reset() { m_handle = Vertex_handle(); m_failed = false; }
+    void reset(const point_pair_t& line)
+    {
+      m_handle = Vertex_handle();
+      m_failed = false;
+      m_line = &line;
+    }
+    
     void operator++(int) { if (!m_failed) checkMapped(); }
     Vertex_handle& operator*() { return m_handle; }
 
     bool isCollinear()
     {
-      checkMapped();
       return m_failed;
     }
 
   private:
+    bool checkIsOwnLinesPoint(Point_2 p) const
+    {
+      return (p.x() == m_line->first.x && p.y() == m_line->first.y)
+        || (p.x() == m_line->second.x && p.y() == m_line->second.y);
+    }
     void checkMapped()
     {
       const auto& p = m_handle->point();
-      auto findIt = m_coordinateToId.find(coordinate_2d_t { p.x(), p.y() });
-      if (findIt != m_coordinateToId.end() && m_assignment.isPointUsed(findIt->second))
-      {
-        m_failed = true;
+      if (!checkIsOwnLinesPoint(Point_2(p.x() / SCALE, p.y() / SCALE)))
+        {
+        auto findIt = m_coordinateToId.find(coordinate_2d_t { p.x() / SCALE, p.y() / SCALE });
+        if (findIt != m_coordinateToId.end() && m_assignment.isPointUsed(findIt->second))
+        {
+          m_failed = true;
+        }
       }
     }
 
   private:
     const VertexAssignment& m_assignment;
     const CoordinateToIdMap& m_coordinateToId;
-    bool m_failed;
+    const point_pair_t* m_line;
+    bool& m_failed;
 
     Vertex_handle m_handle;
 };
@@ -55,17 +71,23 @@ namespace
 {
   Point_2 fromPoint(const Point& p)
   {
-    return Point_2(p.x, p.y);
+    return Point_2(SCALE * p.x, SCALE * p.y);
+  }
+
+  void adjustIfCollinear(Point_2& p1, Point_2& p3)
+  { // assumption p1(x1,y1), p3(x3,y3) with x1 != x3 or y1 != y3 (not the same point)
+    if (p1.x() == p3.x()) p1 = Point_2(p1.x() - 1, p1.y());
+    else if (p1.y() == p3.y()) p1 = Point_2(p1.x(), p3.y() + 1);
   }
 
   bool checkSingleLine(CGAL::Point_set_2<Kernel>& pointset, FakeIterator collinearityChecker, const point_pair_t& line)
   {
-    collinearityChecker.reset();
+    collinearityChecker.reset(line);
     bool firstleft_or_up = (line.first.x < line.second.x ||
         (line.first.x == line.second.x && line.first.y > line.second.y));
     Point_2 p1 = firstleft_or_up ? fromPoint(line.first) : fromPoint(line.second);
     Point_2 p3 = firstleft_or_up ? fromPoint(line.second) : fromPoint(line.first);
-    
+    adjustIfCollinear(p1, p3);
     pointset.range_search(p1, Point_2(p3.x(), p1.y()), p3, Point_2(p1.x(), p3.y()), collinearityChecker);
 
     return collinearityChecker.isCollinear();
@@ -83,11 +105,11 @@ bool gd::trivialCollinearityCheck(const instance_t& instance,
   auto point_range = instance.m_points.getPointIterator();
   for (auto it = point_range.first; it != point_range.second; ++it)
   {
-    pointset.insert(Point_2(it->x, it->y));
+    pointset.insert(Point_2(SCALE * it->x, SCALE * it->y));
     pointMap.insert(it->getCoordToId());
   }
-  
-  FakeIterator collinearityChecker(assignment, pointMap); 
+  bool failed = false;
+  FakeIterator collinearityChecker(assignment, pointMap, failed); 
   
   vertex_t lastFromVertex = VERTEX_UNDEF;
   Point from;
@@ -95,7 +117,7 @@ bool gd::trivialCollinearityCheck(const instance_t& instance,
   {
     if (edge.first < edge.second)
     {
-      if (edge.first != lastFromVertex)
+     if (edge.first != lastFromVertex)
       {
         lastFromVertex = edge.first;
         from = instance.m_points.getPoint(assignment.getAssigned(lastFromVertex));
