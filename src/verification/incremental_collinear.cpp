a3e3b28283823990202c6595d2508d6f126109d7
Keksklauer4 <keksklauer4@gmail.com>
2023-05-17 02:30:11 +0200

    Fixed a bug regarding collinearity.

--- src/verification/incremental_collinear.cpp
+++ src/verification/incremental_collinear.cpp
@@ -9,11 +9,14 @@ IncrementalCollinear::IncrementalCollinear(const Instance& instance,
   const VertexAssignment& assignment, CollinearFunction& collFunc)
   : m_assignment(assignment), m_instance(instance), m_collFunc(collFunc) {}
 
-void IncrementalCollinear::findCollinear(const line_2d_t& line) const
+void IncrementalCollinear::findCollinear(const line_2d_t& line, bool fix_edge)
 {
   for (const auto& p : m_instance.m_points)
   {
-    if (m_assignment.isPointUsed(p.id) && gd::isOnLine(line, p)
-        && !m_collFunc(p)) return;
+    if (gd::isOnLine(line, p))
+    {
+      if(fix_edge) m_invalidPoints.insert(p.id);
+      if (m_assignment.isPointUsed(p.id) && !m_collFunc(p)) return;
+    }
   }
 }
