245676a7b95f9365661830dd6a176e20314e50ea
Ricardo <keksklauer4@gmail.com>
2023-05-27 00:34:21 +0200

    Integration of max sat solver.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <common/instance.hpp>
 #include <io/json_handling.hpp>
 
+#include <EvalMaxSAT.h>
+
 using namespace gd;
 
 int main(int argc, const char** argv)
@@ -20,5 +22,38 @@ int main(int argc, const char** argv)
 
   instance_t instance = parseInstanceFromFile(file);
   std::cout << "Done parsing file." << std::endl;
+
+  unsigned int paralleleThread = 0;
+    auto solver = new EvalMaxSAT(paralleleThread);
+
+    // Create 3 variables
+    int a = solver->newVar();
+    int b = solver->newVar();
+    int c = solver->newVar();
+    
+
+    // Add hard clauses
+    solver->addClause({-a, -b});                // !a or !b
+    auto card = solver->newCard({a, b, c});
+    solver->addClause( {*card <= 2} );          // a + b + c <= 2
+
+    // Add soft clauses
+    solver->addWeightedClause({a, b}, 1);       // a or b
+    solver->addWeightedClause({c}, 1);          // c
+    solver->addWeightedClause({a, -c}, 1);      // a or !c
+    solver->addWeightedClause({b, -c}, 1);      // b or !c
+
+    ////// PRINT SOLUTION //////////////////
+    if(!solver->solve()) {
+        std::cout << "s UNSATISFIABLE" << std::endl;
+        return 0;
+    }
+    std::cout << "s OPTIMUM FOUND" << std::endl;
+    std::cout << "o " << solver->getCost() << std::endl;
+    std::cout << "a = " << solver->getValue(a) << std::endl;
+    std::cout << "b = " << solver->getValue(b) << std::endl;
+    std::cout << "c = " << solver->getValue(c) << std::endl;
+    ///////////////////////////////////////
+
   return 0;
 }
