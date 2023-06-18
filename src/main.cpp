de908925961a6a0335ff4c408b2a872e103ed6fb
Napstylez <bin.bash@gmx.de>
2023-06-18 12:05:16 +0100

    sollte nun bugfrei sein - ist aber langsam

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "gd_types.hpp"
 #include <iostream>
 #include <CLI/CLI.hpp>
 
@@ -5,6 +6,10 @@
 #include <io/json_handling.hpp>
 
 #include <EvalMaxSAT.h>
+#include <iterator>
+#include <ostream>
+#include <vector>
+#include <placement/sat_placement.cpp>
 
 using namespace gd;
 
@@ -23,37 +28,15 @@ int main(int argc, const char** argv)
   instance_t instance = parseInstanceFromFile(file);
   std::cout << "Done parsing file." << std::endl;
 
-  unsigned int paralleleThread = 0;
-    auto solver = new EvalMaxSAT(paralleleThread);
+  std::vector<std::vector<vertex_t>> partitions = {{0, 1, 3, 4}, {2, 5}, {6, 7}, {8}};
+  std::vector<std::vector<point_id_t>> clusters = {{0, 1, 2, 3, 7, 8, 9, 10, 14, 15, 16, 17}, {21, 22, 23, 24, 28, 29, 30, 31}, {4, 5, 6, 11, 12, 13, 18,19, 20}, {25, 26, 27, 32, 33, 34}};
+  std::vector<int> n2p{0, 0, 1, 0, 0, 1, 2, 2, 3};
+  std::vector<int> po2c{0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 1, 1, 1, 1, 3, 3, 3, 1, 1, 1, 1, 3, 3, 3};
+  std::vector<int> p2c{0, 1, 2, 3};
+  std::vector<int> c2p{0, 1, 2, 3};
+  int max_iterations = 500;
+  SATPlacement sat = SATPlacement(instance,partitions, clusters, n2p,po2c, p2c, c2p, max_iterations);
 
-    // Create 3 variables
-    int a = solver->newVar();
-    int b = solver->newVar();
-    int c = solver->newVar();
-    
-
-    // Add hard clauses
-    solver->addClause({-a, -b});                // !a or !b
-    auto card = solver->newCard({a, b, c});
-    solver->addClause( {*card <= 2} );          // a + b + c <= 2
-
-    // Add soft clauses
-    solver->addWeightedClause({a, b}, 1);       // a or b
-    solver->addWeightedClause({c}, 1);          // c
-    solver->addWeightedClause({a, -c}, 1);      // a or !c
-    solver->addWeightedClause({b, -c}, 1);      // b or !c
-
-    ////// PRINT SOLUTION //////////////////
-    if(!solver->solve()) {
-        std::cout << "s UNSATISFIABLE" << std::endl;
-        return 0;
-    }
-    std::cout << "s OPTIMUM FOUND" << std::endl;
-    std::cout << "o " << solver->getCost() << std::endl;
-    std::cout << "a = " << solver->getValue(a) << std::endl;
-    std::cout << "b = " << solver->getValue(b) << std::endl;
-    std::cout << "c = " << solver->getValue(c) << std::endl;
-    ///////////////////////////////////////
 
   return 0;
 }
