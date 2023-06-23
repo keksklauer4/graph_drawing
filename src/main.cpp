1f54af3ce8c4860daf227bec6a3dc82032dca92a
keksklauer4 <keksklauer4@gmail.com>
2023-06-23 23:09:21 +0200

    Revert "Simple CLI"
    This reverts commit 51099ea7297e7c4fa0f51bb1c65c5217ac27878d.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
 #include <iostream>
-#include <CLI/CLI.hpp>
 
 #include <common/verifier.hpp>
-#include <io/json_handling.hpp>
 
-using namespace gd;
-
-int main(int argc, const char** argv)
+int main()
 {
-  CLI::App app("Graph drawing algorithms");
-  // add version output
-  app.set_version_flag("--version", "3.14159265");
-  std::string file;
-  app.add_option("-f,--file,file", file, "File name")->required();
-
-  CLI11_PARSE(app, argc, argv);
-
-  std::cout << "Parsing file " << file << std::endl;
-
-  instance_t instance = parseInstanceFromFile(file);
-  std::cout << "Done parsing file." << std::endl;
+  std::cout << "Hello world!" << std::endl;
   return 0;
 }
