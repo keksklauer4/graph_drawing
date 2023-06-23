30fecdf07d9c27dbd1d3f7f36dd4cf3708c34879
keksklauer4 <keksklauer4@gmail.com>
2023-06-23 23:09:21 +0200

    Revert "Added test graph generation on a grid."
    This reverts commit a00246d35e789b224001a6dc36c6c99fc041866e.

--- prepare.sh
+++ prepare.sh
@@ -1,19 +1,6 @@
 
-#cd external/
-#curl http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz --output lemon.tar.gz
-#tar -xvzf lemon.tar.gz
-#mv lemon-1.3.1/ lemon
-#cd ..
+cd external/
+curl http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz --output lemon.tar.gz
+tar -xvzf lemon.tar.gz
+mv lemon-1.3.1/ lemon
 
-
-cd test
-mkdir gdata
-cd gdata
-mkdir rome
-mkdir temp
-cd temp
-curl http://www.graphdrawing.org/download/rome-graphml.tgz --output rome_graphml.tgz
-tar -xvzf rome_graphml.tgz
-
-cd ../../..
-python3 prepare_graphs.py
